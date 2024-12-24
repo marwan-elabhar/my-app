a101c60225b4c2ec500b59cd4de0c0a667e6c643
MarwanElAbhar <marwan@aklat.io>
2024-12-24 22:29:28 +0200

    implement methods in cart context

--- context/Cart.tsx
+++ context/Cart.tsx
@@ -1,19 +1,47 @@
 'use client'
 
 import React, { createContext, useState, useContext, ReactNode } from 'react';
+import { Product } from '@/components/Card';
 
 interface CartContextType {
-    cartCount: number;
-    setCartCount: React.Dispatch<React.SetStateAction<number>>;
+    cart: Product[];
+    addToCart: (item: Product) => void;
+    removeFromCart: (id: number) => void;
+    clearCart: () => void;
 }
 
 const CartContext = createContext<CartContextType | undefined>(undefined);
 
 export const CartProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const [cartCount, setCartCount] = useState(0);
+    const [cart, setCart] = useState<Product[]>([]);
 
+    const addToCart = (item: Product) => {
+        setCart((cart) => {
+            const existingItem = cart.find((cartItem) => cartItem.id === item.id);
+
+            if (existingItem) {
+                return cart.map((cartItem) =>
+                    cartItem.id === item.id
+                        ? { ...cartItem, quantity: cartItem.quantity + item.quantity }
+                        : cartItem
+                );
+            }
+
+            return [...cart, item];
+        });
+    };
+
+    const removeFromCart = (id: number) => {
+        setCart((cart) => {
+           return cart.filter((cartItem) => cartItem.id !== id)
+        })
+    }
+
+    const clearCart = () => {
+        setCart([])
+    }
     return (
-        <CartContext.Provider value={{ cartCount, setCartCount }}>
+        <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
             {children}
         </CartContext.Provider>
     );
