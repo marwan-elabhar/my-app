34fb3fc6f7f3a90af7b453976e23049fb3eb27aa
MarwanElAbhar <marwan@aklat.io>
2024-12-24 23:31:54 +0200

    fix cart count

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,12 +9,13 @@ export default function Navbar() {
 
     return (
         <nav className="h-[60px] flex items-center bg-gray-200">
-            <div className="flex justify-between items-center w-[1200px] m-auto">
+            <div className="flex justify-between items-center container m-auto">
                 <Link href="/"><p className="text-black font-semibold">Interview Store</p></Link>
                 <div className="flex items-center gap-3">
                     <Link href="/cart"><img src='shopping-cart.svg' />
                     </Link>
-                    <p className="rounded-full text-black bg-gray-300 py-1 px-2 flex justify-center items-center text-xs">{cart.length}</p>
+                    <p className="rounded-full text-black bg-gray-300 py-1 px-2 flex 
+                    justify-center items-center text-xs">{cart.reduce((prev, curr) => prev + curr.quantity, 0)}</p>
                 </div>
             </div>
         </nav>
