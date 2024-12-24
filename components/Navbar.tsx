fc59d3129c286eae59e50af8f79a2f601feada59
MarwanElAbhar <marwan@aklat.io>
2024-12-24 22:29:44 +0200

    route to cart and home page in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,20 @@
 'use client'
 import { useCart } from '../context/Cart';
+import Link from 'next/link'
+
 
 export default function Navbar() {
 
-    const { cartCount } = useCart();
+    const { cart } = useCart();
 
     return (
         <nav className="h-[60px] flex items-center bg-gray-200">
             <div className="flex justify-between items-center w-[1200px] m-auto">
-                <p className="text-black font-semibold">Interview Store</p>
-                <div className="flex items-center gap-6">
-                    <p className="rounded-full text-black bg-gray-300 py-1 px-2 flex justify-center items-center text-xs">{cartCount}</p>
+                <Link href="/"><p className="text-black font-semibold">Interview Store</p></Link>
+                <div className="flex items-center gap-3">
+                    <Link href="/cart"><img src='shopping-cart.svg' />
+                    </Link>
+                    <p className="rounded-full text-black bg-gray-300 py-1 px-2 flex justify-center items-center text-xs">{cart.length}</p>
                 </div>
             </div>
         </nav>
